432fb8372810c3e3b6407b037419e362baf4fde9
matthiasmeissen <blok.artwork@googlemail.com>
2022-02-27 18:31:35 +0100

    Added blink script and serial console numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 
+int number = 1;
+
+void blink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(38400);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Print number counting upwards
+  Serial.println(number);
+  number += 1;
+
+  // Use the blink function to show LED
+  blink();
 }
