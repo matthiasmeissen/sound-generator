c75e3ef0ca1a724cc2d7ec83051bbc89dd1987b8
matthiasmeissen <blok.artwork@googlemail.com>
2022-03-01 23:42:49 +0100

    Added sound to the script

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include <Arduino.h>
 #include <MIDI.h>
+#include <Audio.h>
+#include "SineOscillator.h"
 
+SineOscillator sineOscillator;
+AudioOutputI2S out;
+AudioControlSGTL5000 audioShield;
+AudioConnection patchCord0(sineOscillator,0,out,0);
+AudioConnection patchCord1(sineOscillator,0,out,1);
 
-void blink() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
-}
 
 void makeNote(int note, int duration) {
   usbMIDI.sendNoteOn(note, 127, 1);
@@ -18,15 +19,14 @@ void makeNote(int note, int duration) {
 }
 
 void setup() {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  AudioMemory(2);
+  audioShield.enable();
+  audioShield.volume(0.1);
+
   Serial.begin(38400);
 }
 
 void loop() {
-  // Uses the LED
-  blink();
-
-  // Plays a note
-  makeNote(60, 800);
+  sineOscillator.setParamValue("freq",random(50,1000));
+  delay(50);
 }
