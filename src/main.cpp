5f3dbbb1f9301845e1dc1ea432e5c9a91f46df14
matthiasmeissen <blok.artwork@googlemail.com>
2022-03-04 16:31:56 +0100

    Added the engine and adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
 #include <Arduino.h>
 #include <MIDI.h>
 #include <Audio.h>
-#include "SynthEngine.h"
+#include "SoundEngine.h"
 
-SynthEngine synthEngine;
+SoundEngine soundEngine;
 AudioOutputI2S out;
 AudioControlSGTL5000 audioShield;
-AudioConnection patchCord0(synthEngine,0,out,0);
-AudioConnection patchCord1(synthEngine,0,out,1);
+AudioConnection patchCord0(soundEngine,0,out,0);
+AudioConnection patchCord1(soundEngine,0,out,1);
 
-// SynthEngine Params
+// SoundEngine Params
 // freq - Midi notes converted to Frequency
 // gate - Triggers the envelope
 // gain - Gain of left and right Channels
 
 
 void OnNoteOn(byte channel, byte note, byte velocity) {
-  synthEngine.setParamValue("freq", note);
-  synthEngine.setParamValue("gate", 1);
+  soundEngine.setParamValue("freq", note);
+  soundEngine.setParamValue("gate", 1);
 }
 
 void OnNoteOff(byte channel, byte note, byte velocity) {
-  synthEngine.setParamValue("gate", 0);
+  soundEngine.setParamValue("gate", 0);
 }
 
 void setup() {
